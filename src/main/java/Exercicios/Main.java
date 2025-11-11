052a46daa44a7d273348212ab22c5bae31c42b60
ThaisEvelen <thaisevelen@outlook.com>
2025-11-11 00:21:23 -0300

    estruturas

---
+++ src/main/java/Exercicios/Main.java
@@ -0,0 +1,35 @@
+package Exercicios;
+
+
+
+import Exercicios.Model.Autor;
+import Exercicios.Model.Emprestimo;
+import Exercicios.Model.Livro;
+import Exercicios.Model.Usuario;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Autor autor = new Autor("Jessica Felix", "Brasileira", new Livro[0]);
+
+        Livro livro = new Livro("Java for Beginners", autor, "Tecnologia");
+        livro.setDisponivel(false);
+
+        Usuario usuario = new Usuario("Lucas Rafael", 25, new Emprestimo[0], new Livro[]{livro});
+
+        Date dataRetirada = new Date();
+        Date dataDevolucao = new Date();
+        Emprestimo emprestimo = new Emprestimo(dataRetirada, dataDevolucao, livro, usuario);
+
+        livro.validarDisponibilidade();
+        System.out.println("Livro: " + livro.getTitulo());
+        System.out.println("Autor: " + livro.getAutor().getNome());
+        System.out.println("Gênero: " + livro.getGenero());
+        System.out.println("Usuário: " + usuario.getNome());
+        System.out.println("Idade: " + usuario.getIdade());
+        System.out.println("Data de Retirada: " + emprestimo.getDataRetirada());
+        System.out.println("Data de Devolução: " + emprestimo.getDataDevolucao());
+    }
+}
---
+++ src/main/java/Exercicios/Model/Autor.java
@@ -0,0 +1,40 @@
+package Exercicios.Model;
+
+public class Autor extends Pessoa {
+    private String nacionalidade;
+
+    public Autor(String nome, String nacionalidade, Livro[] livros) {
+        super(nome, livros);
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public Livro[] getObrasPublicadas() {
+        return getLivros();
+    }
+
+    public Livro[] getObrasPublicadasPorGenero(String genero) {
+        Livro[] obras = getLivros();
+        int count = 0;
+
+        for (Livro livro : obras) {
+            if (livro.getGenero().equalsIgnoreCase(genero)) {
+                count++;
+            }
+        }
+
+        Livro[] filtrados = new Livro[count];
+        int index = 0;
+
+        for (Livro livro : obras) {
+            if (livro.getGenero().equalsIgnoreCase(genero)) {
+                filtrados[index++] = livro;
+            }
+        }
+
+        return filtrados;
+    }
+}
